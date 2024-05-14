74f632ba5071def12bc44ad2ec1882513a513564
najmahal25 <jshidalgo@up.edu.ph>
2024-05-14 17:07:26 +0800

    chore: added homepage

--- project/src/App.js
+++ project/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SignInPage from './Pages/SignInPage.js';
-import SignUpPage from './Pages/SignUpPage.js';
+import UsersPage from './Pages/UsersPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<SignInPage />} />
-        <Route path="/SignUpPage" element={<SignUpPage />} /> */}
+        {<Route path="/" element={<UsersPage />} />}
         {/* Add more routes as needed */}
       </Routes>
     </Router>
